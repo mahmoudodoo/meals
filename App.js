16ddfdfbbf563cc075e458ab86e8d8a5bb652aee
mahmoudodoo <mahmoudodoo11@gmail.com>
2020-10-08 14:13:44 +0300

    Adding App Screens

--- App.js
+++ App.js
@@ -1,13 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Font from 'expo-font';
+import { AppLoading } from "expo";
+import MealsNavigator from './navigation/MealsNavigator'
+
+const fetchFonts = () => {
+  return Font.loadAsync({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
+  });
+};
+
 
 export default function App() {
+
+
+  const [fontLoaded,SetFontLoaded] =  useState(false);
+
+
+  if(!fontLoaded){
+    return <AppLoading 
+            startAsync={fetchFonts}
+            onFinish={() => SetFontLoaded(true)}
+    />;
+  };
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+   // <View></View>
+    <MealsNavigator />
   );
 }
 
