48f4ca6bf931b495de54bf17b9003db78d1b0e80
mahmoudodoo <mahmoudodoo11@gmail.com>
2020-10-12 13:46:10 +0300

    Create a custom Text View component and add it to /components/DefaultText.js file. Change some of Text views to DefaultText  in MealItem.js component and MealDetailScreen.js . Design FilterScreen adding four switch buttons to it with a save button in the header.

--- components/MealItem.js
+++ components/MealItem.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { View, Text, StyleSheet, ImageBackground } from 'react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
-
+import DefaultText from '../components/DefaultText'
 
 const MealItem = props => {
     return (
@@ -17,9 +17,9 @@ const MealItem = props => {
 
                     {/* Display  data that comes from other components */}
                     <View style={{ ...styles.mealRow, ...styles.mealDetail }}>
-                        <Text>{props.duration}m</Text>
-                        <Text>{props.complexity.toUpperCase()}</Text>
-                        <Text>{props.affordability.toUpperCase()}</Text>
+                        <DefaultText>{props.duration}m</DefaultText>
+                        <DefaultText>{props.complexity.toUpperCase()}</DefaultText>
+                        <DefaultText>{props.affordability.toUpperCase()}</DefaultText>
                     </View>
 
 
